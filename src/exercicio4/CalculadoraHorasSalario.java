926153f5543b98662380850333bcb36023945944
davispacheco <davipacheco1996@gmail.com>
2022-06-22 14:15:03 -0300

    Adicionar classe CalculadoraHorasSalario, com a resolução do exercício 4.

---
+++ src/exercicio4/CalculadoraHorasSalario.java
@@ -0,0 +1,17 @@
+package src.exercicio4;
+
+import java.util.Scanner;
+
+public class CalculadoraHorasSalario {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Informe qual o valor da sua hora:");
+        double valorHora = input.nextDouble();
+        System.out.println("Informe quantas horas você trabalhou:");
+        double horasTrabalhadas = input.nextDouble();
+        input.close();
+        double salario = valorHora * horasTrabalhadas;
+      System.out.printf("O valor de seu salário mensal é de R$%.2f", salario);
+    }
+}
