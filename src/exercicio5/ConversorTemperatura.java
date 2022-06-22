dd35ca39839dd3b7c6d93972acda80a589fbb95a
davispacheco <davipacheco1996@gmail.com>
2022-06-22 14:41:46 -0300

    Adicionar classe ConversorTemperatura, com a resolução do exercício 5.

---
+++ src/exercicio5/ConversorTemperatura.java
@@ -0,0 +1,16 @@
+package src.exercicio5;
+
+import java.util.Scanner;
+
+public class ConversorTemperatura {
+
+    public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+        System.out.println("Informe a temperatura, em graus Fahrenheit:");
+        double temperatura = input.nextDouble();
+        input.close();
+        double resultado = (temperatura - 32) / 1.8;
+        System.out.println(temperatura + " graus Fahrenheit equivalem a " + resultado + " graus Celsius.");
+    }
+}
