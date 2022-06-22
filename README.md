a80f8b81884fb5c85c44aef778f18cdb6aea0cd1
davispacheco <davipacheco1996@gmail.com>
2022-06-22 11:32:09 -0300

    Adicionar classe Olamundo, com a resolução do exercício 1.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # catalisa4-lista2
 Lista de exercícios com conceitos básicos em JAVA.
+
+1 - Faça um programa que mostre a mensagem "Alô mundo" na tela.
---
+++ src/exercicio1/Olamundo.java
@@ -0,0 +1,7 @@
+package src.exercicio1;
+
+public class Olamundo {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+}
